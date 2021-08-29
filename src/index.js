731a83a087459b9429a31aa576c04b297f342a21
mariia-slobozhan <slobozhan.mi@gmail.com>
2021-08-29 19:25:36 +0300

    fix load more btn

--- src/index.js
+++ src/index.js
@@ -1,73 +1,68 @@
 import './sass/main.scss';
 import apiService from './js/apiService';
-import '../node_modules/material-design-icons/iconfont/material-icons.css'
-import pictureMarkup from './templates/picture.hbs'
-import fullSizePictureOpen from './js/lightbox.js'
+import { form, gallery, loadMorebtn, upBtn } from './js/refs';
+import '../node_modules/material-design-icons/iconfont/material-icons.css';
+import pictureMarkup from './templates/picture.hbs';
 
-const refs = {
-  form: document.querySelector('.search-form'),
-  input: document.querySelector('input[name="query"]'),
-  gallery: document.querySelector('.gallery'),
-  loadMorebtn: document.querySelector('[data-action="load-more"]'),
-  upBtn: document.querySelector('.up-btn'),
-  photoCard: document.querySelector('.photo-card'),
-};
-
-refs.form.addEventListener('submit', querySearch);
-refs.loadMorebtn.addEventListener('click', loadMore);
-refs.upBtn.addEventListener('click', onTop )
-window.addEventListener("scroll", scrollFunction)
-refs.loadMorebtn.classList.add("is-hidden")
-
+form.addEventListener('submit', querySearch);
+loadMorebtn.addEventListener('click', loadMore);
+upBtn.addEventListener('click', onTop);
+window.addEventListener('scroll', scrollFunction);
+loadMorebtn.classList.add('is-hidden');
 
 function querySearch(e) {
   e.preventDefault();
-  
-  apiService.searchQuery = e.currentTarget.elements.query.value;
-  apiService.resetPage()
- 
-  apiService.fetchPictures().then(result => {
-    clearMarkup();
-      createMarkup(result)})
-      refs.loadMorebtn.classList.remove("is-hidden")
+  if (!e.currentTarget.elements.query.value) {
+    return;
+  }
+  loadMorebtn.classList.add('is-hidden');
+  apiService.searchQuery = e.currentTarget.elements.query.value.trim();
+  apiService.resetPage();
+
+  apiService
+    .fetchPictures()
+    .then(result => {
+      clearMarkup();
+      createMarkup(result);
+      return result;
+    })
+    .then(data => {
+      if (data.hits.length > 11) {
+        loadMorebtn.classList.remove('is-hidden');
+      }
+    });
 }
 
 function loadMore(e) {
   apiService.fetchPictures().then(result => {
-      createMarkup(result)})
-
+    createMarkup(result);
+    gallery.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  });
 }
 
-function createMarkup(pictures){
-    const markUp = pictureMarkup(pictures);
-    refs.gallery.insertAdjacentHTML("beforeend", markUp)
-    refs.gallery.scrollIntoView({
-        behavior: 'smooth',
-        block: 'end',
-      });
-
-    //   refs.photoCard.addEventListener('click', fullSizePictureOpen)
+function createMarkup(pictures) {
+  const markUp = pictureMarkup(pictures);
+  gallery.insertAdjacentHTML('beforeend', markUp);
 }
 
 function clearMarkup() {
-    refs.gallery.innerHTML = ''
-
+  gallery.innerHTML = '';
 }
 
-function onTop(e){
-    document.documentElement.scrollTo ({
-        top: 0,
-        behavior: 'smooth',
-      })
+function onTop(e) {
+  document.documentElement.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
 }
-
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-   refs.upBtn.style.display = "block";
+    upBtn.style.display = 'block';
   } else {
-    refs.upBtn.style.display = "none";
+    upBtn.style.display = 'none';
   }
 }
-
-refs.photoCard.addEventListener('click', fullSizePictureOpen)
